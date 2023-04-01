dd0034c5b4026eed949107e3ce64f0b518a6c4eb
JonathanDa16 <daniel1616gomez@outlook.com>
2023-04-01 17:28:20 -0600

    SQLI2

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $sql = "SELECT * FROM users WHERE username=('$username') AND password=('$password')";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
