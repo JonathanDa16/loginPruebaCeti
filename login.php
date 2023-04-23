f6cf666564c51937252d534b44a9225eaeb18981
JonathanDa16 <75548065+JonathanDa16@users.noreply.github.com>
2023-04-23 16:54:20 -0600

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
+    $sql = "SELECT * FROM users WHERE username=? AND password=?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt,'ss', $username, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
     if (mysqli_num_rows($result) > 0) {
         echo "Inicio de sesion exitoso";
